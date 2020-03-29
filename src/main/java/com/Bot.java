8be46ad4517200f092bee05acaea04a6b7465bda
pepya4ka <ndrozdov9@gmail.com>
2020-03-30 02:40:56 +0500

    8 commit

--- src/main/java/com/Bot.java
+++ src/main/java/com/Bot.java
@@ -13,58 +13,9 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.*;
 
 public class Bot extends TelegramLongPollingBot {
-
-
-    private static final String driverName = "com.mysql.jdbc.Driver";
-    private static final String connectionString = "jdbc:mysql://us-cdbr-iron-east-01.cleardb.net";
-    private static final String login = "baab4a4396de37";
-    private static final String password = "3f075f47";
-
-    public static void main(String[] args) {
-
-        try {
-            Class.forName(driverName);
-        } catch (ClassNotFoundException e) {
-            System.out.println("Can't get class. No driver found");
-            e.printStackTrace();
-            return;
-        }
-
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(connectionString, login, password);
-        } catch (SQLException throwables) {
-            System.out.println("Can't get connection. Incorrect URL");
-            throwables.printStackTrace();
-            return;
-        }
-
-        try {
-            connection.close();
-        } catch (SQLException throwables) {
-            System.out.println("Can't close connection");
-            throwables.printStackTrace();
-            return;
-        }
-
-        ApiContextInitializer.init();
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-        try {
-            telegramBotsApi.registerBot(new Bot());
-        } catch (TelegramApiRequestException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void connectToDB() {
-
-    }
 
     public void sendMsg(Message message, String text) {
         SendMessage sendMessage = new SendMessage();
---
+++ src/main/java/com/Main.java
@@ -0,0 +1,63 @@
+package com;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+
+    private final String driverName = "com.mysql.jdbc.Driver";
+    private final String connectionString = "jdbc:mysql://us-cdbr-iron-east-01.cleardb.net";
+    private final String login = "baab4a4396de37";
+    private final String password = "3f075f47";
+
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+    }
+
+    public void run() {
+        //connectToDB();
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void connectToDB() {
+        try {
+            Class.forName(driverName);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Can't get class. No driver found");
+            e.printStackTrace();
+            return;
+        }
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(connectionString, login, password);
+        } catch (SQLException throwables) {
+            System.out.println("Can't get connection. Incorrect URL");
+            throwables.printStackTrace();
+            return;
+        }
+
+        try {
+            connection.close();
+        } catch (SQLException throwables) {
+            System.out.println("Can't close connection");
+            throwables.printStackTrace();
+            return;
+        }
+    }
+
+}
